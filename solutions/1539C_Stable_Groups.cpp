b51da3a69d3a5ef0e0417cfd5bc652c576a236b8
rheallyc <vireonxi@gmail.com>
2021-06-11 18:52:12 +0000

    Solve 1539C: Stable Groups

---
+++ solutions/1539C_Stable_Groups.cpp
@@ -0,0 +1,36 @@
+// Problem: Stable Groups
+// URL: https://codeforces.com/problemset/problem/1539/C
+// Rating: 1200
+// Tags: greedy, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+ll k, x, a[210000];
+multiset<ll> s;
+
+int main(){
+    scanf("%d%lld%lld", &n, &k, &x);
+    for(int i=1; i<=n; i++){
+        scanf("%lld", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    int ans=1;
+    for(int i=2; i<=n; i++){
+        if(a[i]-a[i-1]<=x) continue;
+        ans++;
+        ll num=(a[i]-a[i-1])/x;
+        if((a[i]-a[i-1])%x==0) num--;
+        s.insert(num);
+    }
+    for(auto t:s){
+        if(k>=t){
+            k-=t;
+            ans--;
+        }
+        else break;
+    }
+    printf("%d\n", ans);
+}
+
