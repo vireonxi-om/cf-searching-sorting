205876dda0487e73c3009e20c66061ff1aececb4
rheallyc <vireonxi@gmail.com>
2021-08-03 02:31:13 +0000

    Solve 1526A: Mean Inequality

---
+++ solutions/1526A_Mean_Inequality.cpp
@@ -0,0 +1,30 @@
+// Problem: Mean Inequality
+// URL: https://codeforces.com/problemset/problem/1526/A
+// Rating: 800
+// Tags: constructive algorithms, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[60];
+
+void solve(){
+    scanf("%d", &n);
+    for(int i=1; i<=2*n; i++){
+        scanf("%d", &a[i]);
+    }
+    sort(a+1, a+2*n+1);
+    for(int i=1; i<=n; i++){
+        printf("%d %d ", a[2*n+1-i], a[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
