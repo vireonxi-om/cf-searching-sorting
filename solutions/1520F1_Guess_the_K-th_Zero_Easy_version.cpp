e93e97da3a7e29e832e1a9bd4f55ce9ec0171695
rheallyc <vireonxi@gmail.com>
2023-04-22 16:54:32 +0000

    Solve 1520F1: Guess the K-th Zero (Easy version)

---
+++ solutions/1520F1_Guess_the_K-th_Zero_Easy_version.cpp
@@ -0,0 +1,28 @@
+// Problem: Guess the K-th Zero (Easy version)
+// URL: https://codeforces.com/problemset/problem/1520/F1
+// Rating: 1600
+// Tags: binary search, interactive
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, t, k;
+
+
+int main(){
+    scanf("%d%d%d", &n, &t, &k);
+    int l=1, r=n, ans, num;
+    while(l<=r){
+        int mid=(l+r)/2;
+        printf("? %d %d\n", 1, mid);
+        fflush(stdout);
+        scanf("%d", &num);
+        if(mid-num==k) ans=mid;
+        if(mid-num>=k) r=mid-1;
+        else l=mid+1;
+    }
+    printf("! %d\n", ans);
+    return 0;
+}
+
+
