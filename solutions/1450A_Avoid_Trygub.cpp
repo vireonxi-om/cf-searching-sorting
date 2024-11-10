041cac7191dcf5da896c10a2edf1b12c272f5f9b
rheallyc <vireonxi@gmail.com>
2024-11-10 01:33:23 +0000

    Solve 1450A: Avoid Trygub

---
+++ solutions/1450A_Avoid_Trygub.cpp
@@ -0,0 +1,34 @@
+// Problem: Avoid Trygub
+// URL: https://codeforces.com/problemset/problem/1450/A
+// Rating: 800
+// Tags: constructive algorithms, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+char s[210];
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    scanf("%s", &s);
+    int cnt=0;
+    vector<char> b;
+    for(int i=0; i<n; i++){
+        if(s[i]=='t') cnt++;
+        else b.push_back(s[i]);
+    }
+    while(cnt>0){
+        b.push_back('t');
+        cnt--;
+    }
+    for(auto x:b) printf("%c", x);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}
